a2d54a489946afc5b26ab57324c782d53a11215d
marijapanic <mpanic432@gmail.com>
2022-06-22 23:42:58 +0200

    Add repository pattern, Automapper

---
+++ Contracts/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelListing.API.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetAllAsync();
+        Task<T> GetAsync(int id);
+
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task<T> AddSync(T entity);
+
+        Task<bool> Exists(int id);
+    }
+}
