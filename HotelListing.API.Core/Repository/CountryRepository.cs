4845a368b49459f8f654f5ab12820c46d74ffcd2
marijapanic <mpanic432@gmail.com>
2023-01-04 23:05:58 +0100

    Restructure

---
+++ HotelListing.API.Core/Repository/CountryRepository.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using HotelListing.API.Contracts;
+using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Repository
+{
+    public class CountryRepository : GenericRepository<Country>, ICountriesRepository
+    {
+        private readonly HotelListingDbContext _context;
+
+        public CountryRepository(HotelListingDbContext context, IMapper mapper) : base(context, mapper)
+        {
+            _context = context;
+        }
+
+        public async Task<Country> GetDetails(int id)
+        {
+            return await _context.Countries.Include(q => q.Hotels).FirstOrDefaultAsync(q => q.Id == id);
+        }
+    }
+}
