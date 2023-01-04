700c5002689d57e2f0f143c2d0559eec57552227
marijapanic <mpanic432@gmail.com>
2023-01-04 22:40:38 +0100

    Extract to Core layer

--- Models/Country/BaseCountryDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HotelListing.API.Models.Country
-{
-    public abstract class BaseCountryDto
-    {
-        public string Name { get; set; }
-        public string ShortName { get; set; }
-    }
-}
